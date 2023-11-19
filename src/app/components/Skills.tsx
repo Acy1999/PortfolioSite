b470155c66e8f1f31698e70d899ce6b1ee3e2a5a
Tony Young <acy1999@gmail.com>
2023-11-19 09:29:40 -0500

    Updated skills

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -12,9 +12,7 @@ export default function Skills() {
               Skills &amp; Technologies
             </h1>
             <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi sit
-              ipsa delectus eum quo voluptas aspernatur accusantium distinctio
-              possimus est.
+            Navigating the Digital Landscape with Proficiency and Precision. Explore a Diverse Array of Skills Showcased in My Portfolio
             </p>
           </div>
           <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
